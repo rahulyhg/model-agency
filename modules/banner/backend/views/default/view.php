534fa9c6127cce5884c0942decb295d4b737ce1f
Bikmanson <bikmanson@gmail.com>
2018-08-23 22:31:19 +0300

    change crated_at and updated_at format to datetime in view

--- modules/banner/backend/views/default/view.php
+++ modules/banner/backend/views/default/view.php
@@ -9,32 +9,44 @@ use yii\widgets\DetailView;
 $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Banners', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
+$formatter = \Yii::$app->formatter;
 ?>
 <div class="banner-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
+      <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+      <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        'class' => 'btn btn-danger',
+        'data' => [
+          'confirm' => 'Are you sure you want to delete this item?',
+          'method' => 'post',
+        ],
+      ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'name',
-            'text:ntext',
-            'position',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
+  <?= DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+      'id',
+      'name',
+      'text:ntext',
+      'position',
+      [
+        'attribute' => 'created_at',
+        'value' => function ($model) {
+          return \Yii::$app->formatter->asDatetime($model->created_at);
+        }
+      ],
+      [
+        'attribute' => 'updated_at',
+        'value' => function ($model) {
+          return \Yii::$app->formatter->asDatetime($model->updated_at);
+        }
+      ]
+    ],
+  ]) ?>
 
 </div>
