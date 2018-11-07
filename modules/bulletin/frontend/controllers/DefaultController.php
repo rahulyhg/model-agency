f3a695fa42d8fb33f8e345512f72ff86e8491103
Your Name <you@example.com>
2018-11-07 16:59:29 +0200

    add single ads to bulletin module. add getPhone action. add BulletinStat

--- modules/bulletin/frontend/controllers/DefaultController.php
+++ modules/bulletin/frontend/controllers/DefaultController.php
@@ -4,14 +4,56 @@ namespace modules\bulletin\frontend\controllers;
 
 
 use modules\bulletin\common\models\Bulletin;
+use modules\bulletin\common\models\BulletinStat;
 use modules\bulletin\common\models\Category;
 use modules\bulletin\frontend\forms\FilterForm;
+use modules\client\common\models\Client;
 use yii\data\ActiveDataProvider;
+use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
+
+  /**
+   * Sing ads
+   * @param $id
+   * @return string
+   * @throws NotFoundHttpException
+   */
+  public function actionView($id)
+  {
+    $model = $this->findModel($id);
+    $stat = BulletinStat::findOne(['bulletin_id' => $model->id]);
+    $stat->views = $stat->views + 1;
+    $stat->save();
+    return $this->render('view', [
+      'model' => $model,
+      'stat' => $stat
+    ]);
+  }
+
+  /**
+   * Action for ajax requests only
+   * @param $id client ID
+   * @return string
+   * @throws BadRequestHttpException
+   * @throws NotFoundHttpException
+   */
+  public function actionGetPhone($id)
+  {
+    if( !\Yii::$app->request->isAjax ) {
+      throw new BadRequestHttpException('Only ajax requests are enabled!');
+    }
+
+    $client = Client::findOne($id);
+    if(!$client) {
+      throw new NotFoundHttpException('Client not found!');
+    }
+
+    return $client->phone;
+  }
 
   public function actionCategory($id)
   {
@@ -36,10 +78,18 @@ class DefaultController extends Controller
   {
     $ids = [$id];
     $tmpIds = Category::find()->where(['parent_id' => $id])->column();
-    while(!empty($tmpIds)){
+    while (!empty($tmpIds)) {
       $ids = array_merge($ids, $tmpIds);
       $tmpIds = Category::find()->where(['parent_id' => $tmpIds])->column();
     }
     return $ids;
+  }
+  protected function findModel($id)
+  {
+    if (($model = Bulletin::findOne($id)) !== null) {
+      return $model;
+    }
+
+    throw new NotFoundHttpException('The requested page does not exist.');
   }
 }
