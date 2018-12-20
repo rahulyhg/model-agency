7528534b4ead125f01120d941c2df3d6b02cfec8
bikmanson <bikmanson@gmail.com>
2018-12-20 13:21:14 +0200

    add file input

--- modules/mod/backend/views/default/_form.php
+++ modules/mod/backend/views/default/_form.php
@@ -1,5 +1,6 @@
 <?php
 
+use kartik\widgets\FileInput;
 use modules\mod\common\models\EyesColor;
 use modules\mod\common\models\HairColor;
 use yii\helpers\Html;
@@ -15,78 +16,86 @@ use backend\widgets\crudActions\CrudActions;
 <div class="mod-form">
 
   <?php $form = ActiveForm::begin([
-  'defaultLangInd' => $model->getDefaultLangInd(),
+    'defaultLangInd' => $model->getDefaultLangInd(),
   ]); ?>
 
-  <div class="m-portlet__head">
-    <div class="m-portlet__head-caption">
-      <?= CrudActions::widget(["model" => $model, "template" => "{index}"]) ?>
+    <div class="m-portlet__head">
+        <div class="m-portlet__head-caption">
+          <?= CrudActions::widget(["model" => $model, "template" => "{index}"]) ?>
+        </div>
+        <div class="m-portlet__head-tools">
+            <ul class="m-portlet__nav">
+                <li class="m-portlet__nav-item">
+                  <?= Html::dropDownList(null, $model->getDefaultLangInd(), $model->getLangMap(), [
+                    'class' => 'form-control',
+                    'id' => 'lang-dropdown',
+                  ]) ?>
+                </li>
+                <li class="m-portlet__nav-item">
+                  <?= CrudActions::widget(['model' => $model,]); ?>
+                </li>
+            </ul>
+        </div>
     </div>
-    <div class="m-portlet__head-tools">
-      <ul class="m-portlet__nav">
-        <li class="m-portlet__nav-item">
-          <?= Html::dropDownList(null, $model->getDefaultLangInd(), $model->getLangMap(), [
-          'class' => 'form-control',
-          'id' => 'lang-dropdown',
-          ]) ?>
-        </li>
-        <li class="m-portlet__nav-item">
-          <?= CrudActions::widget(['model' => $model,]); ?>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="m-portlet__body">
-    <div class="row">
-      <div class="col-xl-8 offset-xl-2">
-          <div class="row">
-            <div class="col-md-6">
-              <?= $form->field($model, 'bust')->textInput() ?>
+    <div class="m-portlet__body">
+        <div class="row">
+            <div class="col-xl-8">
+                <div class="row">
+                    <div class="col-md-6">
+                      <?= $form->field($model, 'bust')->textInput() ?>
+                    </div>
+                    <div class="col-md-6">
+                      <?= $form->field($model, 'waist')->textInput() ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                      <?= $form->field($model, 'hips')->textInput() ?>
+                    </div>
+                    <div class="col-md-6">
+                      <?= $form->field($model, 'eyes_color_id')->widget(kartik\widgets\Select2::class, [
+                        'data' => EyesColor::getMap(),
+                        'options' => ['placeholder' => ''],
+                        'pluginOptions' => ['allowClear' => true],
+                      ]) ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                      <?= $form->field($model, 'hair_color_id')->widget(kartik\widgets\Select2::class, [
+                        'data' => HairColor::getMap(),
+                        'options' => ['placeholder' => ''],
+                        'pluginOptions' => ['allowClear' => true],
+                      ]) ?>
+                    </div>
+                    <div class="col-md-6">
+                      <?= $form->field($model, 'shoes')->textInput() ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                      <?= $form->field($model->variationModels, 'first_name')->textInput(['maxlength' => true]) ?>
+                    </div>
+                    <div class="col-md-6">
+                      <?= $form->field($model->variationModels, 'middle_name')->textInput(['maxlength' => true]) ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                      <?= $form->field($model->variationModels, 'last_name')->textInput(['maxlength' => true]) ?>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-6">
-              <?= $form->field($model, 'waist')->textInput() ?>
+            <div class="col-cl-4">
+              <?= $form->field($model, 'images')->widget(FileInput::class, [
+                'options' => [
+                  'accept' => 'image/*',
+                  'multiple' => true,
+                ]
+              ]) ?>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <?= $form->field($model, 'hips')->textInput() ?>
-            </div>
-            <div class="col-md-6">
-              <?= $form->field($model, 'eyes_color_id')->widget(kartik\widgets\Select2::class, [
-                      'data' => EyesColor::getMap(),
-                      'options' => ['placeholder' => ''],
-                      'pluginOptions' => ['allowClear' => true],
-                    ]) ?>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <?= $form->field($model, 'hair_color_id')->widget(kartik\widgets\Select2::class, [
-                      'data' => HairColor::getMap(),
-                      'options' => ['placeholder' => ''],
-                      'pluginOptions' => ['allowClear' => true],
-                    ]) ?>
-            </div>
-            <div class="col-md-6">
-              <?= $form->field($model, 'shoes')->textInput() ?>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <?= $form->field($model->variationModels, 'first_name')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-6">
-              <?= $form->field($model->variationModels, 'middle_name')->textInput(['maxlength' => true]) ?>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <?= $form->field($model->variationModels, 'last_name')->textInput(['maxlength' => true]) ?>
-            </div>
-          </div>
-      </div>
-    </div>
-    <?php ActiveForm::end(); ?>
+        </div>
+      <?php ActiveForm::end(); ?>
 
-  </div>
+    </div>
 </div>
--- modules/mod/backend/views/default/index.php
+++ modules/mod/backend/views/default/index.php
@@ -12,49 +12,48 @@ $this->title = 'Mod';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="m-portlet__head">
-  <div class="m-portlet__head-caption">
-    <?= \backend\widgets\multipleDelete\MultipleDelete::widget([
-      'gridId' => 'mod-grid',
-    ]) ?>
-  </div>
-  <div class="m-portlet__head-tools">
-    <ul class="m-portlet__nav">
-      <li class="m-portlet__nav-item">
-        <a href="<?= Url::to(['create']) ?>"
-           class="btn btn-accent m-btn m-btn--custom m-btn--pill m-btn--icon m-btn--air">
+    <div class="m-portlet__head-caption">
+      <?= \backend\widgets\multipleDelete\MultipleDelete::widget([
+        'gridId' => 'mod-grid',
+      ]) ?>
+    </div>
+    <div class="m-portlet__head-tools">
+        <ul class="m-portlet__nav">
+            <li class="m-portlet__nav-item">
+                <a href="<?= Url::to(['create']) ?>"
+                   class="btn btn-accent m-btn m-btn--custom m-btn--pill m-btn--icon m-btn--air">
 						<span>
 							<i class="la la-plus"></i>
 							<span>Создать</span>
 						</span>
-        </a>
-      </li>
-    </ul>
-  </div>
+                </a>
+            </li>
+        </ul>
+    </div>
 </div>
 <div class="m-portlet__body">
-  <div class="dataTables_wrapper">
-    <div class="row">
-      <div class="col-sm-12">
-                          <?= GridView::widget([
-          'id' => 'mod-grid',
-          'options' => ['class' => 'dataTable'],
-          'dataProvider' => $dataProvider,
-          'filterModel' => $searchModel,
-        'columns' => [
-          ['class' => \backend\lib\CheckboxColumn::class],
-                      'id',
-            'bust',
-            'waist',
-            'hips',
-            'eyes_color_id',
-            //'hair_color_id',
-            //'shoes',
-            //'created_at',
-            //'updated_at',
-          ['class' => \backend\lib\ActionColumn::class],
-          ],
-          ]); ?>
-                      </div>
+    <div class="dataTables_wrapper">
+        <div class="row">
+            <div class="col-sm-12">
+              <?= GridView::widget([
+                'id' => 'mod-grid',
+                'options' => ['class' => 'dataTable'],
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
+                  ['class' => \backend\lib\CheckboxColumn::class],
+                  'bust',
+                  'waist',
+                  'hips',
+                  'eyes_color_id',
+                  //'hair_color_id',
+                  //'shoes',
+                  //'created_at',
+                  //'updated_at',
+                  ['class' => \backend\lib\ActionColumn::class],
+                ],
+              ]); ?>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
