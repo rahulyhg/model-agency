1960f98d5c5d5cc9ac1f64b5b6f68dbb20ff89f3
Your Name <you@example.com>
2019-01-22 12:49:10 +0200

    page module in development

--- app/modules/page/frontend/views/default/view.php
+++ app/modules/page/frontend/views/default/view.php
@@ -3,8 +3,8 @@
  * @var $this \yii\web\View
  * @var $model \modules\page\common\models\Page
  */
-$this->title = $model->seo_title;
-$this->params['meta_description'] = $model->seo_description;
-$this->params['breadcrumbs'][] = $model->title;
+//$this->title = $model->seo_title;
+//$this->params['meta_description'] = $model->seo_description;
+//$this->params['breadcrumbs'][] = $model->title;
 ?>
 <h1>In development</h1>
