813f72a7a75f00cd8280f55e8eef6e8ef2700b3a
Your Name <you@example.com>
2019-03-04 16:29:51 +0200

    like cookie

--- app/modules/like/widgets/like/controllers/LikeWidgetController.php
+++ app/modules/like/widgets/like/controllers/LikeWidgetController.php
@@ -3,6 +3,7 @@
 namespace modules\like\widgets\like\controllers;
 
 use modules\like\common\models\Like;
+use Yii;
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\web\Response;
@@ -30,6 +31,7 @@ class LikeWidgetController extends Controller
       'user_id' => \Yii::$app->user->id ?: null
     ]);
     if ($like->save()) {
+      $this->setCookie($entity, $entityId);
       return [
         'status' => 'success',
         'count' => Like::find()->where(['entity_id' => $entityId, 'entity' => $entity])->count()
@@ -41,14 +43,43 @@ class LikeWidgetController extends Controller
     ];
   }
 
+  /**
+   * @param string $entity
+   * @param int $entityId
+   */
+  protected function setCookie(string $entity, int $entityId)
+  {
+    $cookies = Yii::$app->response->cookies;
+    $cookies->add(new \yii\web\Cookie([
+      'name' => "like-$entity-$entityId",
+      'value' => true,
+    ]));
+  }
+
+  protected function likeCookieExist($entity, $entityId)
+  {
+    return isset($_COOKIE["like-$entity-$entityId"]);
+  }
+
+  /**
+   * @param string $entity
+   * @param int $entityId
+   * @return bool
+   */
   protected function canCurrentUserAddLike(string $entity, int $entityId): bool
   {
+    // check by user
     $user = \Yii::$app->user->identity;
     if($user && Like::findOne(['user_id' => $user->id, 'entity_id' => $entityId, 'entity' => $entity])) {
       return false;
     }
+    // check by ip
     $ip = \Yii::$app->request->userIP;
     if($ip && Like::findOne(['ip' => $ip, 'entity_id' => $entityId, 'entity' => $entity])) {
+      return false;
+    }
+    // check by cookie
+    if($this->likeCookieExist($entity, $entityId)) {
       return false;
     }
     return true;
